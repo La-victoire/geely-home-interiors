80f66b6e375c696a40e06a5db4e8326006cbdd4a
Victory Oghuvbu <104170646+La-victoire@users.noreply.github.com>
2025-12-13 00:31:15 +0100

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,15 @@
+// app/robots.ts
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: "/dashboard" // or any admin route you want hidden
-    },
-    sitemap: `${process.env.NEXT_PUBLIC_APP_URL}/sitemap.xml`
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: "/dashboard", // hide admin pages
+      },
+    ],
+    sitemap: `${process.env.NEXT_PUBLIC_APP_URL}/sitemap.xml`,
   };
 }
